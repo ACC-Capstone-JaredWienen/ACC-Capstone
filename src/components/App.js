0e26d5538f5494ceb408f57db4afe22d64139b63
Jared <126293208+indohippi@users.noreply.github.com>
2023-09-06 14:53:44 -0500

    removed filter from all tabs so its only on the home page

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,19 @@ import ProductDetailPage from './ProductDetailPage';
 import CartPage from './CartPage';
 import UserLoginPage from './UserLoginPage';
 import UserProfilePage from './UserProfilePage';
-import CategoryFilter from './CategoryFilter';
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <div style={{ padding: '20px' }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:productId" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/login" element={<UserLoginPage />} />
-          <Route path="/profile" element={<UserProfilePage />} />
-        </Routes>
-      </div>
-      <CategoryFilter />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:productId" element={<ProductDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<UserLoginPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
     </Router>
   );
 }
