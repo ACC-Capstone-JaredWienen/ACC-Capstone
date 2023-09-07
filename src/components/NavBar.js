965002c47261f8c19967e98bb8b76fef93c372da
Jared <126293208+indohippi@users.noreply.github.com>
2023-09-07 15:37:58 -0500

    fixed NavBar to properly render products for each category

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,14 @@ const NavBar = () => {
   return (
     <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px', background: '#f4f4f4' }}>
       <Link to="/" style={{ textDecoration: 'none', color: '#333' }}>Home</Link>
-      <Link to="/products" style={{ textDecoration: 'none', color: '#333' }}>Products</Link>
+      
+      {/* Dropdown or Submenu might be better for a real-world application, but for simplicity, let's use direct links */}
+      <Link to="/products" style={{ textDecoration: 'none', color: '#333' }}>All Products</Link>
+      <Link to="/products?category=electronics" style={{ textDecoration: 'none', color: '#333' }}>Electronics</Link>
+      <Link to="/products?category=jewelery" style={{ textDecoration: 'none', color: '#333' }}>Jewelery</Link>
+      <Link to="/products?category=mens-clothing" style={{ textDecoration: 'none', color: '#333' }}>Men's Clothing</Link>
+      <Link to="/products?category=womens-clothing" style={{ textDecoration: 'none', color: '#333' }}>Women's Clothing</Link>
+      
       <Link to="/cart" style={{ textDecoration: 'none', color: '#333' }}>Cart</Link>
       {/* Add more links as needed */}
     </nav>
--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -2,22 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ProductCard from './ProductCard';
 
-const useQuery = () => {
-  return new URLSearchParams(useLocation().search);
-};
-
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
+  const location = useLocation();
+  
+  // A utility function to parse query parameters from the URL.
+  function useQuery() {
+    return new URLSearchParams(location.search);
+  }
+
   const query = useQuery();
-  const category = query.get("category");
+  let category = query.get('category'); // This will get the category value from the URL
+
+  // Converts URL-friendly category to API format
+  const getCategoryForAPI = (categorySlug) => {
+    switch (categorySlug) {
+      case 'mens-clothing':
+        return "men's clothing";
+      case 'womens-clothing':
+        return "women's clothing";
+      default:
+        return categorySlug;
+    }
+  };
+  category = getCategoryForAPI(category);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then(res => res.json())
       .then(data => {
         if (category) {
+          // If there's a category query parameter, filter the products
           setProducts(data.filter(product => product.category === category));
         } else {
+          // Otherwise, set all products
           setProducts(data);
         }
       });
@@ -25,7 +43,7 @@ const ProductsPage = () => {
 
   return (
     <div>
-      <h1>{category ? `${category.charAt(0).toUpperCase() + category.slice(1)} Products` : "All Products"}</h1>
+      <h1>{category ? `${category.charAt(0).toUpperCase() + category.slice(1)} Products` : 'All Products'}</h1>
       <div>
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
