293a3f49c235e186641aeba9b0b450cbb316c9cb
Jared <126293208+indohippi@users.noreply.github.com>
2023-09-07 22:14:55 -0500

    adjusted navbar to include login and profile page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,13 +5,15 @@ const HomePage = () => {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h1>Welcome to our Online Store!</h1>
-      <p>Filter by Category:</p>
+      <p>Browse Categories:</p>
       <ul>
+        <li><Link to="/products">All Products</Link></li>
         <li><Link to="/products?category=electronics">Electronics</Link></li>
-        <li><Link to="/products?category=clothing">Clothing</Link></li>
-        <li><Link to="/products?category=home-kitchen">Home & Kitchen</Link></li>
-        <li><Link to="/products?category=books">Books</Link></li>
-        <li><Link to="/products?category=toys">Toys</Link></li>
+        <li><Link to="/products?category=jewelery">Jewelry</Link></li>
+        <li><Link to="/products?category=mens-clothing">Men's Clothing</Link></li>
+        <li><Link to="/products?category=womens-clothing">Women's Clothing</Link></li>
+        <li><Link to="/login">User Login</Link></li>
+        <li><Link to="/profile">User Profile</Link></li>
       </ul>
       {/* Add other introductory or promotional material here */}
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,21 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px', background: '#f4f4f4' }}>
-      <Link to="/" style={{ textDecoration: 'none', color: '#333' }}>Home</Link>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', background: '#f4f4f4' }}>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Link to="/" style={{ textDecoration: 'none', color: '#333' }}>Home</Link>
+        <Link to="/products" style={{ textDecoration: 'none', color: '#333' }}>All Products</Link>
+        <Link to="/products?category=electronics" style={{ textDecoration: 'none', color: '#333' }}>Electronics</Link>
+        <Link to="/products?category=jewelery" style={{ textDecoration: 'none', color: '#333' }}>Jewelry</Link>
+        <Link to="/products?category=mens-clothing" style={{ textDecoration: 'none', color: '#333' }}>Men's Clothing</Link>
+        <Link to="/products?category=womens-clothing" style={{ textDecoration: 'none', color: '#333' }}>Women's Clothing</Link>
+        <Link to="/cart" style={{ textDecoration: 'none', color: '#333' }}>Cart</Link>
+      </div>
       
-      {/* Dropdown or Submenu might be better for a real-world application, but for simplicity, let's use direct links */}
-      <Link to="/products" style={{ textDecoration: 'none', color: '#333' }}>All Products</Link>
-      <Link to="/products?category=electronics" style={{ textDecoration: 'none', color: '#333' }}>Electronics</Link>
-      <Link to="/products?category=jewelery" style={{ textDecoration: 'none', color: '#333' }}>Jewelery</Link>
-      <Link to="/products?category=mens-clothing" style={{ textDecoration: 'none', color: '#333' }}>Men's Clothing</Link>
-      <Link to="/products?category=womens-clothing" style={{ textDecoration: 'none', color: '#333' }}>Women's Clothing</Link>
-      
-      <Link to="/cart" style={{ textDecoration: 'none', color: '#333' }}>Cart</Link>
-      {/* Add more links as needed */}
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Link to="/login" style={{ textDecoration: 'none', color: '#333' }}>Login</Link>
+        <Link to="/profile" style={{ textDecoration: 'none', color: '#333' }}>Profile</Link>
+      </div>
     </nav>
   );
 };
